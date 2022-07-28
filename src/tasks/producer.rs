28d1e7aa2de99ba531f8c1cc22115c662ca97fef
Pierre Tondereau <pierre.tondereau@protonmail.com>
2022-07-28 17:20:22 +0200

    Add test for transaction producer.

--- src/tasks/producer.rs
+++ src/tasks/producer.rs
@@ -38,3 +38,47 @@ impl<R: AsyncRead + Unpin + Send> TransactionProducer<R> {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::transaction::{Transaction, TransactionKind};
+
+    use super::*;
+
+    use rust_decimal_macros::dec;
+
+    #[tokio::test]
+    async fn test_deserialize_csv() -> Result<()> {
+        let tests = vec![
+            b"\
+type,client,tx,amount
+deposit,1,1,1.664
+"
+            .as_slice(),
+            b"\
+type,       client,    tx,  amount
+deposit,         1,     1,  1.664
+"
+            .as_slice(),
+        ];
+
+        let expected_tx_cmd =
+            Transaction::new(TransactionKind::Deposit, 1, 1, dec!(1.664)).try_into()?;
+        for data in tests.into_iter() {
+            let (sender, mut receiver) = mpsc::channel(1);
+            let producer = TransactionProducer::new(data, sender);
+
+            producer.start().await?;
+
+            let cmd = receiver.recv().await.expect("cmd has not been received");
+            match cmd {
+                PaymentEngineCommand::TransactionCommand(tx_cmd) => {
+                    assert_eq!(tx_cmd, expected_tx_cmd)
+                }
+                _ => unreachable!(),
+            }
+        }
+
+        Ok(())
+    }
+}
